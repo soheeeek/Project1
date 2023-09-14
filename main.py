b03daab048fb8f4c91700d3a06dafca2f539a457
Sohee <soheeee96@naver.com>
2023-09-14 15:16:59 +0900

    커밋3 빼기 함수 추가

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ def add_func(n1 , n2) :
     res = n1+n2
     return res
 
+def sub_func(n1, n2) :
+    res = n1-n2
+    return res
 
 num1 = 100
 num2 = 200
@@ -9,3 +12,7 @@ num2 = 200
 result = add_func(num1, num2)
 
 print(num1, '+' , num2 , '=', result)
+
+result = sub_func(num1, num2)
+
+print(num1, '-' , num2 , '=', result)
